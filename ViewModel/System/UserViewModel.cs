1e23ea16eb69d8b48622de83cad7f11a8e519668
Quoc Tran Anh <trananhquoc22720@gmail.com>
2022-06-14 16:11:19 +0700

    delete email when registersa

--- ViewModel/System/UserViewModel.cs
+++ ViewModel/System/UserViewModel.cs
@@ -33,7 +33,6 @@ namespace ViewModel.System
     public class RegisterRequest
     {
         public string UserName { get; set; }
-        public string Email { get; set; }
         public string Password { get; set; }
 
     }
